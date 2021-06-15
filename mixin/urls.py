e8c33cf6ef16474b6be3cb87289147e4d214772b
Chirag Sharma <chiragsharmachaahat@gmail.com>
2021-06-15 09:19:15 +0530

    Added the staff login

--- mixin/urls.py
+++ mixin/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name = 'admin'),
     path('class/', include('class.urls') ),
     path('', include('users.urls')),
 ]
